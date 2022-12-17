e35dfc2e3eec8995168b492692d89c2c1447abba
willianglitch <nailliw901@gmail.com>
2022-12-17 13:00:28 -0300

    adding the first challenge

---
+++ CHALLENGES/FRONTEND-MENTOR-CARD/src/js/main.js
@@ -0,0 +1,31 @@
+
+
+
+document.querySelector('#App').innerHTML = `
+<div class="card">
+    <div class="card-image">
+        <img src="./src/assets/image-product-desktop.jpg" alt=" imagem do perfume Gabrielle Chanel">
+    </div>
+
+    <div class="card-Content">
+        <main>
+            <p class="title">perfume</p>
+            <h1>Gabrielle Essence Eau De Parfum</h1>
+            <p class="info">
+                A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.
+            </p>
+            <div class="Price">
+                <p>$149,99</p> <small>$169,99</small>
+            </div>
+            
+            <div class="buttonAddCart">
+                <button>
+                    <div class="cart"></div>
+                    <p>Add to Cart</p>
+                </button>
+            </div>
+
+        </main>
+    </div>
+</div>
+`
